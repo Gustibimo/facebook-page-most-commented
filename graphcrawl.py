7e696f51cdcf7b08eb1deaefb7ed5e52834ce9d8
gbimo <gusti.bimo@detik.com>
2018-01-03 11:37:17 +0700

    "count comment"

--- graphcrawl.py
+++ graphcrawl.py
@@ -5,13 +5,13 @@ import pprint
 
 # get Facebook access token from environment variable
 ACCESS_TOKEN = "EAACEdEose0cBABnjvubPKRVMKheRXuUE751QLfoYmSBJPL6Fw6AGGEkQgvQmhDEQ8rkQd1Df0n3ThOl9q3abJR5EYBzLjBMZBiuXoTvhgY07VlG9Q5i5JjCHSkfl6cNcOnDMX0gBNcGpgOiclZACiLG131Bc3rJH6WvM7o3RZAUlJG98zeBBmBkyEeZBGV4ZD"
-ids = "673366896048573"
+ids = "1212796532105604"
 # build the URL for the API endpoint
 host = "https://graph.facebook.com/"
-path = ids+"/feed"
+path = ids+"?fields=comments{id,message,like_count,comment_count}"
 token = urllib.urlencode({"access_token": ACCESS_TOKEN})
 
-url = "{host}{path}?{params}".format(host=host, path=path, params=token)
+url = "{host}{path}&{params}".format(host=host, path=path, params=token)
 
 # open the URL and read the response
 resp = urllib.urlopen(url).read()
